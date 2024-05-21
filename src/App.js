579d4c866a80d7e700382a478e74f6139135991f
cuduy7 <duydang468@gmail.com>
2024-05-22 00:35:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My App</h1>
+        <Button variant="contained" color="primary">
+          My Material-UI Button
+        </Button>
+        <Button variant="outlined" color="secondary" size="large">
+          Custom Button
+        </Button>
       </header>
     </div>
   );
