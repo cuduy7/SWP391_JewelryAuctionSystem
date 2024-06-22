6b03451abb2ef94acdc8e24a7a5eec609cecdf31
XTDV2021 <xuanthudoanvo@gmail.com>
2024-06-22 16:12:24 +0700

    thu update

--- app/components/providers/format/Background.tsx
+++ app/components/providers/format/Background.tsx
@@ -1,16 +1,14 @@
 import Head from 'next/head';
-import { BackgroundProps } from "@/types"
+import { BackgroundProps } from '@/types';
 
-const Background: React.FC<BackgroundProps> = ({
-    src,
-    children
-}) => {
-    return (
-        <>
-            <Head>
-                <link rel="preload" as="image" href={src} />
-            </Head>
-            <div className="
+const Background: React.FC<BackgroundProps> = ({ src, children }) => {
+  return (
+    <>
+      <Head>
+        <link rel="preload" as="image" href={src} />
+      </Head>
+      <div
+        className="
                     relative
                     inset-0
                     lg:inset-auto
@@ -23,13 +21,14 @@ const Background: React.FC<BackgroundProps> = ({
                     duration-500
                     overflow-x-hidden
                 "
-                style={{
-                    backgroundImage: `url(${src})`,
-                }}>
-                {children}
-            </div>
-        </>
-    )
-}
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 128, 0, 0.7), rgba(0, 0, 255, 0.7)), url(${src})`,
+        }}
+      >
+        {children}
+      </div>
+    </>
+  );
+};
 
-export default Background
+export default Background;
